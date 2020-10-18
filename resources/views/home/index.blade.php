77d4c3c7aac6e9dd4f91c000b835a3cd9d3e0a23
Faga Davide <faga.davide@gmail.com>
2020-10-18 19:04:56 +0200

    Initial import

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layout.app')
+@section('content')
+<main role="main" class="container mt-5">
+    <h1 class="mt-5">Charo Arc</h1>
+    <p class="lead">Single, not sorry !</p>
+</main>
+@endsection
---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>Library</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  </head>
+
+  <body>
+    <header>
+      <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">Charo Arc</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="{{route("home")}}">Home </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#">Link-1</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#">Link-2</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+
+    @yield('content')
+  </body>
+</html>
