fce9cefc57b09bc88cb7113092c279e0b2efe0b5
Faga Davide <faga.davide@gmail.com>
2020-12-10 20:34:47 +0100

    test

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if($this->app->environment('production')) 
+        {
+            \URL::forceScheme('https');
+        }
         //
     }
 }
