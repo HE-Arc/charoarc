5b0b7bd38d4f1754dc8f47562ad7a4660bcd37cc
Paysant Adrien <adrien.paysant@he-arc.ch>
2020-12-12 21:30:20 +0100

    function index du controller match est devenue légère.

--- app/Http/Controllers/MatchController.php
+++ app/Http/Controllers/MatchController.php
@@ -20,95 +20,12 @@ class MatchController extends Controller
     {
         if(Auth::check())
             {
-                $id=Auth::id();
-                $userMe=User::getUserById($id);
-                $userMatchs=Match::getAllMatchByUser($id);
-                $matchsToAnswer=[];
-                $indexMatchsToAnswer=0;
-                $oneMatchToAnswer=null;
-                $newMatchUserId=null;
-
-                foreach($userMatchs as $m){
-                    if($m->user_id1==$id){
-                        $user2=User::getUserById($m->user_id2);
-                        //cas on est user 1
-                        if($m->status_user1 == false && $m->is_done==false){
-                            //on n'a pas encore voté on propose donc le match si respect regles
-                            if($userMe->gender== $user2->interessedBy && $user2->gender== $userMe->interessedBy){
-                                //on correspond
-                                $matchsToAnswer[$indexMatchsToAnswer++]=$m;
-                            }
-                            else{
-                                $m->is_done=true;
-                            }
-                        }
-                    }
-                    else if($m->user_id2==$id){
-                        $user1=User::getUserById($m->user_id1);
-                        //cas on est user 2
-                        if($m->status_user2 == false && $m->is_done==false){
-                            //on n'a pas encore voté on propose donc le match si respect regles
-                            if($userMe->gender== $user1->interessedBy && $user1->gender== $userMe->interessedBy){
-                                //on correspond
-                                $matchsToAnswer[$indexMatchsToAnswer++]=$m;
-                            }
-                            else{
-                                $m->is_done=true;
-                            }
-                        }
-                    }
-                }
-                if($indexMatchsToAnswer>0){
-                    //on peut avoir un match a répondre
-                    //on choisit donc un match dans la liste
-                    $oneMatchToAnswer=$matchsToAnswer[random_int(0,$indexMatchsToAnswer-1)];
-                }
-                else{
-                    //pas de matchs, il faut proposer un nouveau
-                    $possibleUsersToMatchWith=[];
-                    $indexPossibleMatchs=0;
-                    foreach(User::allUser() as $userTargeted){
-                        if($userMe->interessedBy == $userTargeted->gender && $userMe!=$userTargeted && $userTargeted->interessedBy == $userMe->gender ){
-                            $bool=true;
-                            foreach(Match::getAllMatchByUser($id) as $m){
-                                if(($userTargeted->id==$m->user_id1 && $id==$m->user_id2) || ($userTargeted->id==$m->user_id2 && $id==$m->user_id1))
-                                    $bool = false;
-                            }
-                            if($bool)
-                                $possibleUsersToMatchWith[$indexPossibleMatchs++]=$userTargeted;
-                        }
-                    }
-                    if($indexPossibleMatchs>0){
-                        $newMatchUserId=$possibleUsersToMatchWith[random_int(0,$indexPossibleMatchs-1)]->id;
-                     }
-                }
-             
-                $image='defaultUser.jpg';
-                $idToSend=null;
-                $name=null;
-                if($oneMatchToAnswer!=null){//cas match existant
-                    $image=$oneMatchToAnswer->getTargetImage($id)->image;
-                    $idToSend=$oneMatchToAnswer->id;
-                    $name=Match::getMatchById($idToSend)->getUserNameTargetFromIdLogged($id);
-                }
-                else if($newMatchUserId!=null){//cas creation possible d'un futur match
-                    $image=User::getUserById($newMatchUserId)->image;
-                    $name=User::getUserById($newMatchUserId)->name;
-                }
-
-                //for debug ONLY TOBEREMOVED
-                if($oneMatchToAnswer!=null && false){
-                     //fordebug
-                     $oneMatchToAnswer=new Match();
-                     $oneMatchToAnswer->user_id1=$id;
-                     $oneMatchToAnswer->user_id2=$newMatchUserId;
-                     $oneMatchToAnswer->status_user1=false;
-                     $oneMatchToAnswer->status_user2=false;
-                     $oneMatchToAnswer->is_done=false;
-                    echo "<br><br><br>";
-                    echo $oneMatchToAnswer->toString();
-                }
-                
+                $tabTarget=Match::findATarget();
+                $userMatchs=$tabTarget[0];
+                $idToSend=$tabTarget[1];
+                $newMatchUserId=$tabTarget[2];
+                $image=$tabTarget[3];
+                $name=$tabTarget[4];
                 return view('match.matchs', ["userMatchs"=>$userMatchs,"matchToAnswerId"=>$idToSend,"newMatchUserId"=>$newMatchUserId,"image"=>$image,"name"=>$name],);
             }
             return redirect()->route('login');
--- app/Models/Match.php
+++ app/Models/Match.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 
 class Match extends Model
@@ -66,6 +67,100 @@ class Match extends Model
     } 
 
     //static
+
+    public static function findATarget(){
+        $id=Auth::id();
+        $userMe=User::getUserById($id);
+        $userMatchs=Match::getAllMatchByUser($id);
+        $matchsToAnswer=[];
+        $indexMatchsToAnswer=0;
+        $oneMatchToAnswer=null;
+        $newMatchUserId=null;
+
+        foreach($userMatchs as $m){
+            if($m->user_id1==$id){
+                $user2=User::getUserById($m->user_id2);
+                //cas on est user 1
+                if($m->status_user1 == false && $m->is_done==false){
+                    //on n'a pas encore voté on propose donc le match si respect regles
+                    if($userMe->gender== $user2->interessedBy && $user2->gender== $userMe->interessedBy){
+                        //on correspond
+                        $matchsToAnswer[$indexMatchsToAnswer++]=$m;
+                    }
+                    else{
+                        $m->is_done=true;
+                    }
+                }
+            }
+            else if($m->user_id2==$id){
+                $user1=User::getUserById($m->user_id1);
+                //cas on est user 2
+                if($m->status_user2 == false && $m->is_done==false){
+                    //on n'a pas encore voté on propose donc le match si respect regles
+                    if($userMe->gender== $user1->interessedBy && $user1->gender== $userMe->interessedBy){
+                        //on correspond
+                        $matchsToAnswer[$indexMatchsToAnswer++]=$m;
+                    }
+                    else{
+                        $m->is_done=true;
+                    }
+                }
+            }
+        }
+        if($indexMatchsToAnswer>0){
+            //on peut avoir un match a répondre
+            //on choisit donc un match dans la liste
+            $oneMatchToAnswer=$matchsToAnswer[random_int(0,$indexMatchsToAnswer-1)];
+        }
+        else{
+            //pas de matchs, il faut proposer un nouveau
+            $possibleUsersToMatchWith=[];
+            $indexPossibleMatchs=0;
+            foreach(User::allUser() as $userTargeted){
+                if($userMe->interessedBy == $userTargeted->gender && $userMe!=$userTargeted && $userTargeted->interessedBy == $userMe->gender ){
+                    $bool=true;
+                    foreach(Match::getAllMatchByUser($id) as $m){
+                        if(($userTargeted->id==$m->user_id1 && $id==$m->user_id2) || ($userTargeted->id==$m->user_id2 && $id==$m->user_id1))
+                            $bool = false;
+                    }
+                    if($bool)
+                        $possibleUsersToMatchWith[$indexPossibleMatchs++]=$userTargeted;
+                }
+            }
+            if($indexPossibleMatchs>0){
+                $newMatchUserId=$possibleUsersToMatchWith[random_int(0,$indexPossibleMatchs-1)]->id;
+             }
+        }
+     
+        $image='defaultUser.jpg';
+        $idToSend=null;
+        $name=null;
+        if($oneMatchToAnswer!=null){//cas match existant
+            $image=$oneMatchToAnswer->getTargetImage($id)->image;
+            $idToSend=$oneMatchToAnswer->id;
+            $name=Match::getMatchById($idToSend)->getUserNameTargetFromIdLogged($id);
+        }
+        else if($newMatchUserId!=null){//cas creation possible d'un futur match
+            $image=User::getUserById($newMatchUserId)->image;
+            $name=User::getUserById($newMatchUserId)->name;
+        }
+
+        //for debug ONLY TOBEREMOVED
+        if($oneMatchToAnswer!=null && false){
+             //fordebug
+             $oneMatchToAnswer=new Match();
+             $oneMatchToAnswer->user_id1=$id;
+             $oneMatchToAnswer->user_id2=$newMatchUserId;
+             $oneMatchToAnswer->status_user1=false;
+             $oneMatchToAnswer->status_user2=false;
+             $oneMatchToAnswer->is_done=false;
+            echo "<br><br><br>";
+            echo $oneMatchToAnswer->toString();
+        }
+
+        return Collect([$userMatchs,$idToSend,$newMatchUserId,$image,$name]);
+    }
+
 
     public static function allMatchs()
     {
