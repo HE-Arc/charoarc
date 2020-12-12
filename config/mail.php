3530396c99ae9518010ffa11ec4bf25f4fa11c5a
Faga Davide <faga.davide@gmail.com>
2020-12-12 22:04:26 +0100

    ajout systeme de recuperation mot depasse

--- config/mail.php
+++ config/mail.php
@@ -36,11 +36,11 @@ return [
     'mailers' => [
         'smtp' => [
             'transport' => 'smtp',
-            'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
-            'port' => env('MAIL_PORT', 587),
+            'host' => env('MAIL_HOST', 'smtp.gmail.org'),
+            'port' => env('MAIL_PORT', 25),
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
-            'username' => env('MAIL_USERNAME'),
-            'password' => env('MAIL_PASSWORD'),
+            'username' => env('MAIL_USERNAME', "charoarc.noreply@gmail.com"),
+            'password' => env('MAIL_PASSWORD', "Omnia Vincit Amor"),
             'timeout' => null,
             'auth_mode' => null,
         ],
@@ -84,8 +84,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'charoarc.noreply@gmail.com'),
+        'name' => env('MAIL_FROM_NAME', 'charoarc.noreplym'),
     ],
 
     /*
