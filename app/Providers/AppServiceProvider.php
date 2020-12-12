ceef48bc8bc481d1f8a5d23783108b3a9b9c52ad
Faga Davide <faga.davide@gmail.com>
2020-12-12 20:36:20 +0100

    corriger probleme http to https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,7 +25,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if(\App::environment('production'))
+        if (env('APP_ENV') !== 'local') 
         {
             URL::forceScheme('https');
         }
