fc20512d348dbb940b16664f20e9a4409ded0aea
Paysant Adrien <adrien.paysant@he-arc.ch>
2020-12-13 22:54:06 +0100

    trustproxies edit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,9 +25,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {  
-        if (\App::environment() !== "local") 
-        {
-            URL::forceScheme('https');
-        }
+        // if (\App::environment() !== "local") 
+        // {
+            URL::forceScheme('http');
+        // }
     }
 }
