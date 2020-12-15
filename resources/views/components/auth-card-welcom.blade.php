43317702bba3edc7e2d736fc661c46ccc89188bb
Faga Davide <faga.davide@gmail.com>
2020-12-15 21:21:42 +0100

    logo position fix

--- resources/views/components/auth-card-welcom.blade.php
+++ resources/views/components/auth-card-welcom.blade.php
@@ -1,7 +1,7 @@
 <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100" style="--bg-opacity: 1;
     background: rgb(180,131,189);
     background: linear-gradient(225deg, rgba(180,131,189,1) 21%, rgba(116,222,244,1) 67%, rgba(192,160,205,1) 86%);">
-    <div style="display:flex; flex-direction: row;justify-content:space-around; vertical-align: baseline; display:flex; flex-direction: column;justify-content:space-around;width:55%">
+    <div style="display:flex; flex-direction: row;justify-content:space-around; vertical-align: baseline; display:flex; flex-direction: column;justify-content:space-around;width:30%">
         {{ $logo }}
     </div>
     <div style="display:flex;  flex-wrap: wrap; flex-direction: row;justify-content:space-around;">
