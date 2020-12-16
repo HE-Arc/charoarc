aedb4b37f719d5d8ced75c71318940c436255b65
Paysant Adrien <adrien.paysant@he-arc.ch>
2020-12-16 10:41:58 +0100

    correction dans affichage matchs=>matches

--- resources/views/match/matchs.blade.php
+++ resources/views/match/matchs.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
 <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight" style="padding:1em;">
-            {{ __('Matchs') }}
+            {{ __('Matches') }}
         </h2>
     </x-slot>
     <x-slot name="slot"> 
