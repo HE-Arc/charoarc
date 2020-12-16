3de2847a58148afa3c3e327e1df6ac654774aa6a
Faga Davide <faga.davide@gmail.com>
2020-12-16 15:29:18 +0100

    add comment on userController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,13 +10,19 @@
     use App\Rules\HashCheck;
 
     
-
+/**
+ * UserController
+ * manager between modelUser and viewUser
+ */
     class UserController extends Controller
     {
-        public function profile()
-        {
-            if(Auth::check())
-            {
+        /**
+         * profile
+         * take value from user and send to view
+         * if the user is not authentified, redir to login page
+         */
+        public function profile() {
+            if(Auth::check()) {
                 $id=Auth::id();
                 $user = User::getUserById($id);
                 return view('user.profile', ["user"=>$user]);
@@ -24,83 +30,81 @@
             return redirect()->route('login');
         }
 
-        public function update(Request $request)
-        {    
+        /**
+         * update
+         * valide the request and send the value to model user
+         */
+        public function update(Request $request) {    
             $request->validate(['id' => 'required|exists:App\Models\User,id']);
             $id = $request->input('id');
            
-            if ($request->has('Name')) 
-            {
+            if ($request->has('Name')) {
                 $request->validate(['Name' => 'required|min:3|max:50']);
                 $name = $request->input('Name');
                 $request->session()->flash('alert-success', 'Name updated !');
                 User::updateUserName($id, $name);
             }
-            if ($request->has('Birthday')) 
-            {
+            if ($request->has('Birthday')) {
                 $request->validate(['Birthday' => 'required|date']);
                 $birthday = $request->input('Birthday');
                 $request->session()->flash('alert-success', 'Birthday updated !');
                 User::updateUserBirthday($id, $birthday);
             }
-            if ($request->has('description')) 
-            {
+            if ($request->has('description')) {
                 $request->validate(['description' => 'required|string|max:140']);
                 $description = $request->input('description');
                 $request->session()->flash('alert-success', 'Description updated !');
                 User::updateUserDescription($id, $description);
             }
-            if ($request->has('Email')) 
-            {
+            if ($request->has('Email')) {
                 $request->validate(['Email' => 'required|email|unique:users,email']);
                 $email = $request->input('Email');
                 $request->session()->flash('alert-success', 'Email updated !');
                 User::updateUserEmail($id, $email);
             }
-            if ($request->has('Gender')) 
-            {
+            if ($request->has('Gender')) {
                 $request->validate(['Gender' => 'required|string']);
                 $gender = $request->input('Gender');
                 $request->session()->flash('alert-success', 'Gender updated !');
                 User::updateUserGender($id, $gender);
             }
-            if ($request->has('InteressedBy')) 
-            {
+            if ($request->has('InteressedBy')) {
                 $request->validate(['InteressedBy' => 'required|string']);
                 $interessedBy = $request->input('InteressedBy');
                 $request->session()->flash('alert-success', 'interested updated !');
                 User::updateUserInteressedBy($id, $interessedBy);
             }
-            if ($request->has('CurrentPassword','Password', 'ConfirmePassword')) 
-            {
+            if ($request->has('CurrentPassword','Password', 'ConfirmePassword')) {
                 $request->validate([
                     'CurrentPassword' => [
                         'required', 
                         new HashCheck()
                     ]
                 ]);
-                $request->validate([
-                     'CurrentPassword'=>'min:6|max:30|required',
-                    'Password' => 'min:6|max:30|required_with:ConfirmePassword|same:ConfirmePassword',/*|regex:/[a-z]/|regex:/[A-Z]/|regex:/[0-9]/|regex:/[@$!%*#?&]/*//* !!! we are all too lazy to apply this during an exercise !!!*/
+                /*
+                password can be more complexe with
+                |regex:/[a-z]/|regex:/[A-Z]/|regex:/[0-9]/|
+                regex:/[@$!%*#?&]/*/
+                /* !!! we are all too lazy to apply this during an exercise !!!*/
+                $request->validate(['CurrentPassword'=>'min:6|max:30|required',
+                    'Password' => 'min:6|max:30|required_with:ConfirmePassword|same:ConfirmePassword',
                     'ConfirmePassword' => 'min:6|max:30' ]);
                 $password = $request->input('Password');
                 $request->session()->flash('alert-success', 'Password updated !');
                 User::updateUserPassword($id, $password);
             }
-            if($request->has('phone')){
+            if($request->has('phone')) {
                 $request->validate(['phone' => 'required|regex:/^([0-9\s\-\+\(\)]*)$/|min:10']);
                 $request->session()->flash('alert-success', 'Phone updated !');
                 User::updatePhone($id,$request->input('phone'));
             }
-            if ($request->hasFile('Image'))
-            {
+            if ($request->hasFile('Image')) {
                 $request->validate(['Image' => 'required|image|max:10240']);
                 $path = $request->Image->store('public');
                 $image = $request->Image->hashName();
                 //del old img
                 $user = User::getUserById($id);
-                if($user->image != "defaultUser.jpg")
-                {
+                if($user->image != "defaultUser.jpg") {
                     Storage::delete('public/'. $user->image);
                 }
                 //update new img
@@ -109,9 +113,5 @@
             }
         
             return redirect()->route('profile');
-
-            //DEBUG PAGE -> use this and comment the redirection return
-            //return view('user.debugPage',["debugRequest"=>$request]);
-
         } 
     }
