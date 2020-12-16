e818f2c71d6faa2f83cd92255d7999f9631d2104
Paysant Adrien <adrien.paysant@he-arc.ch>
2020-12-16 11:24:25 +0100

    ajout icone en haut de la page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-
+        <link rel="shortcut icon" href="{{ asset('storage/logo.ico') }}">
         <title>{{ config('app.name') }}</title>
 
         <!-- Fonts -->
